7cd42ea116819fd61c072a4ac7c76c2c28d77a05
elsieeva <eethomas4@wisc.edu>
2026-04-29 15:22:57 +0200

    basic bpm converter

---
+++ script.js
@@ -0,0 +1,53 @@
+function convertMileTime(timeString){
+
+  const parts = timeString.split(":");
+
+  const minutes = Number(parts[0]);
+
+  const seconds = Number(parts[1]);
+
+  return minutes + (seconds / 60);
+}
+
+function calculateBPM(height, mileTime){
+
+  const strideLength = height * 0.413;
+
+  const strideFeet = strideLength / 12;
+
+  const walkingStepsPerMile =
+    5280 / strideFeet;
+
+  const runningStepsPerMile =
+    walkingStepsPerMile / 1.28;
+
+  const cadence =
+    runningStepsPerMile / mileTime;
+
+  const bpm =
+    cadence / 2;
+
+  return Math.round(bpm);
+}
+
+const button =
+  document.getElementById("generateBtn");
+
+button.addEventListener("click", function(){
+
+  const height =
+    Number(document.getElementById("height").value);
+
+  const mileTimeString =
+    document.getElementById("mileTime").value;
+
+  const mileTime =
+    convertMileTime(mileTimeString);
+
+  const bpm =
+    calculateBPM(height, mileTime);
+
+  document.getElementById("result").textContent =
+    `Target BPM: ${bpm}`;
+
+});
